b53f97cb4749464a762b2acfc06b5400643f15eb
AliAkbar_Zohour <71524940+aliakbarzohour@users.noreply.github.com>
2022-01-31 12:35:47 +0330

    commenting

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-// add a listener for all window
+// ---------------------------------------------------------------
+// In this part of JavaScript coding, we should try to slow down 
+// the image scrolling rate as the window scrolls.
+// ---------------------------------------------------------------
+
 window.addEventListener("scroll", function() {
+    // I take the picture
     const parallax = document.querySelector("img");
+    // In this section, I tell JavaScript to be sensitive to how 
+    // much this image scrolls and tell me how much it scrolls.
     let scrollP = window.pageYOffset;
+    // And finally I say slow down the scrolling of the pixels.
     parallax.style.transform = 'translateY(' + scrollP * .3 + 'px)';
 })
